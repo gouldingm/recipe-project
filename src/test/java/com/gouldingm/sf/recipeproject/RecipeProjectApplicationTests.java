ce16acaf6fb34d3f59d6f89612be769fe3929790
Michael Goulding <goulding.michael@gmail.com>
2021-01-28 15:04:03 +0000

    Initial commit

---
+++ src/test/java/com/gouldingm/sf/recipeproject/RecipeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gouldingm.sf.recipeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
